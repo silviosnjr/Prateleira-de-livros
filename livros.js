e706624dc7131d636e5de295bb1274e86aaf774d
silviosnjr <silviosnjr@gmail.com>
2025-03-24 01:55:05 -0300

    first commit

---
+++ livros.js
@@ -0,0 +1,51 @@
+livrosSelecionados = [{
+    titulo : "Clara dos Anjos",
+    link : "http://www.dominiopublico.gov.br/pesquisa/DetalheObraForm.do?select_action=&co_obra=2060",
+    imgCapa: "Clara dos Anjos.jpg"
+},
+{
+    titulo : "O Mulato",
+    link : "http://www.dominiopublico.gov.br/pesquisa/DetalheObraForm.do?select_action=&co_obra=16537",
+    imgCapa: "O Mulato.jpg"
+},
+{
+    titulo : "Iracema",
+    link : "http://www.dominiopublico.gov.br/pesquisa/DetalheObraForm.do?select_action=&co_obra=1844",
+    imgCapa: "Iracema.jpg"
+},
+{
+    titulo : "Tarde",
+    link : "http://www.dominiopublico.gov.br/pesquisa/DetalheObraForm.do?select_action=&co_obra=1999",
+    imgCapa: "Tarde.jpg"
+},
+{
+    titulo : "Dom Casmurro",
+    link : "http://www.dominiopublico.gov.br/pesquisa/DetalheObraForm.do?select_action=&co_obra=2081",
+    imgCapa: "Dom Casmurro.jpg"
+},
+{
+    titulo : "O Cortiço",
+    link : "http://www.dominiopublico.gov.br/pesquisa/DetalheObraForm.do?select_action=&co_obra=1723",
+    imgCapa: "O Cortiço.jpg"
+},
+{
+    titulo : "O Alienista",
+    link : "http://www.dominiopublico.gov.br/pesquisa/DetalheObraForm.do?select_action=&co_obra=1939",
+    imgCapa: "O Alienista.jpg"
+},
+{
+    titulo : "A chave do tamanho",
+    link : "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_40ccd21099164aacaa200d03c56ebb77.pdf",
+    imgCapa: "A chave do tamanho.jpg"
+},
+{
+    titulo : "Contos",
+    link : "https://5ca0e999-de9a-47e0-9b77-7e3eeab0592c.usrfiles.com/ugd/5ca0e9_59e6cfd30cc74cca90b3db292e00aa18.pdf",
+    imgCapa: ""
+},
+{
+    titulo : "Vidas Secas",
+    link : "https://archivepublicdomain.com/files/2025/01/vidas-secas.pdf",
+    imgCapa: "Vidas Secas.jpg"
+}
+];
